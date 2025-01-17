335fbca3d77f2779a28cf408d27cf679200de9f3
meashishpatel <ashishpatel3009@gmail.com>
2025-01-17 13:37:49 +0530

    Refactor import statement for React in App.jsx

--- Integration/src/App.jsx
+++ Integration/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import VideoPlayer from "./Components/VideoPlayer";
 import axios from "axios";
 import ProgressBar from "./components/ProgressBar";
